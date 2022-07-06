85e5277129fa85ce284b1332fb4aed487b6dde8e
phonsekal2 <108601647+phonsekal2@users.noreply.github.com>
2022-07-06 09:51:33 +0700

    Update home.py

--- home.py
+++ home.py
@@ -1,12 +1,7 @@
 import streamlit as st
 import pandas as pd
-from PIL import Image
+
 
 APP_TITLE = "Dede Saputra App"
-st.set_page_config(
-    page_title=APP_TITLE,
-    page_icon=Image.open("./utils/omic_learn.ico"),
-    layout="centered",
-    initial_sidebar_state="auto",
-)
+
 st.title("Tampak Depan")
